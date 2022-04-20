dd064e49ac1032297b77cb0f9dd94244c6a3866f
ANA ASCUA <anaascua5@yahoo.com.ar>
2022-04-19 22:13:51 -0300

    mi primer commit en git

---
+++ Java/Lección 1/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,18 @@
+
+public class HolaMundo {
+    public static void main(String[]args) {
+        System.out.println("Hola Mundo desde Java!");
+    
+        //int miVariable;
+        int miVariable = 10;
+        System.out.println(miVariable);
+        miVariable = 5;
+        System.out.println(miVariable);
+      
+        // TIPO STRING
+        String miVariableCadena = "Bienvenidos";
+        System.out.println(miVariableCadena);
+        miVariableCadena = "Sigamos creciendo en programación";
+        System.out.println(miVariableCadena);
+    }    
+}
---
+++ Python/Lección/main.py
@@ -0,0 +1,16 @@
+miVariable = 3
+print(miVariable)
+miVariable = "a"
+print(miVariable)
+miVariable = 3.5
+print(miVariable)
+miVariable = "ESTO ES MAGIA!"
+print(miVariable)
+x = 10
+y = 2
+z = x + y
+print(id(x))
+# Las literales se escriben x800 (los últimos 3 números del n° gigante)
+print(id(y))
+# Los números de memoria son volátiles, se esfuman cada vez que ejecutamos de nuevo
+print(id(z))
