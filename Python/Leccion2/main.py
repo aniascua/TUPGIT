a2fded3d88dccdd58d7c189f6ff8315aa5af1f14
ANA ASCUA <anaascua5@yahoo.com.ar>
2022-05-18 22:54:29 -0300

    Actualizando la carpeta Python

---
+++ Python/Leccion2/main.py
@@ -0,0 +1,8 @@
+# En esta clase veremos la sentencia if/else
+condicion = False
+if condicion == True:
+    print('Condicion Verdadera')
+elif condicion == False:
+    print('Condicion Falsa')
+else:
+    print('Condición sin especificar')
