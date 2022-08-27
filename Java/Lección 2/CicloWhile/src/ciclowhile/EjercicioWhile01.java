222e848286d4b9d40e067edc52fc92138d03be7b
ANA ASCUA <anaascua5@yahoo.com.ar>
2022-08-27 13:52:43 -0300

    Ingresamos las clases de Java, con ciclos while, do while y for

---
+++ Java/Lección 2/CicloWhile/src/ciclowhile/EjercicioWhile01.java
@@ -0,0 +1,29 @@
+package ciclowhile;
+
+/**
+ *
+ * @author ASCUA
+ */
+public class EjercicioWhile01 {
+
+    public static void main(String[] args) {
+      //  WHILE. SE EJECUTA SI SE CUMPLE LA CONDICIÓN
+//        var conteo = 0;  // inferencia de tipos
+//        while (conteo < 7) {  // mientras conteo sea menor a 7, imprimir lo del sout
+//            System.out.println("conteo =  " + conteo);
+//            conteo++;    // vamos aumentando en uno la variable
+//        }
+        //  DO WHILE. EJECUTA 1 VEZ ANTES DE EVALUAR LA CONDICIÓN
+        var contador = 0;
+        do{
+            System.out.println("contador =  " + contador);
+            contador++;
+        }while(contador < 7);
+        
+        // FOR. SE REPITE LAS VECES QUE LE INDIQUEMOS. 
+        // INICIALIZA Y CREA VARIABLES.
+        
+        
+        
+    }
+}
