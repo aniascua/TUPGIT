a29651875d650d04da1ce40c1d56da77a5def529
ANA ASCUA <anaascua5@yahoo.com.ar>
2022-10-28 16:32:04 -0300

    Subo ejercicios de JavaScript, operadores, mayor de edad, número par o impar

---
+++ Javascript/Lección03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,19 @@
+// Clase 7 JavaScript
+// Ejercicio para encontrar números pares:
+
+var numero = 0;
+
+/*while (numero <= 100) {
+    if (numero % 2 == 0) {
+        console.log(numero);
+    }
+    numero++;
+}
+*/
+
+let parImpar = 22;
+if(parImpar % 2 == 0){
+    console.log("El número es par");
+}else{
+    console.log("El número es impar");
+}
---
+++ Javascript/Lección03/03-02-MayorDeEdad.js
@@ -0,0 +1,11 @@
+// Clase 7 JavaScript
+// Ejercicio Mayor de edad:
+
+let edad = 22;
+
+if(edad >= 18){
+    console.log("Eres mayor de edad");
+}else{
+    console.log("Eres menor de edad");
+}   
+
