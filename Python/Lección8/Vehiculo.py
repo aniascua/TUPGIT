cfb0a74ece6722c80d559ec1303a4fa7cbcecf74
ANA ASCUA <anaascua5@yahoo.com.ar>
2022-10-28 15:51:32 -0300

    Subiendo el ejercicio de la clase Vehiculo: vehiculo, auto, bicicleta Python

--- Python/Lección8/Vehiculo.py
+++ Python/Lección8/Vehiculo.py
@@ -1,5 +1,6 @@
 # ASCUA Ana
 # Clase 11 - 25/10/2022
+
 class Vehiculo:
     """
     Definir una clase padre llamada Vehículo y dos clases hijas llamadas
@@ -20,3 +21,41 @@ class Vehiculo:
 
     Crear un objeto de cada clase
     """
+
+    def __init__(self, color, ruedas):
+        self.color = color
+        self.ruedas = ruedas
+
+    def __str__(self):
+        return 'Color del vehículo: ' + self.color + ', cantidad de Ruedas: ' + str(self.ruedas)
+
+
+class Auto(Vehiculo):
+    def __init__(self, color, ruedas, velocidad):
+        super().__init__(color, ruedas)
+        self.velocidad = velocidad
+
+    def __str__(self):
+        return super().__str__() + ', Velocidad: ' + str(self.velocidad) + ' km/h.'
+
+
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas, tipo):
+        super().__init__(color, ruedas)
+        self.tipo = tipo
+
+    def __str__(self):
+        return super().__str__() + ', Tipo de bici: ' + self.tipo
+
+
+# Primer objeto de la clase padre Vehículo:
+vehiculo = Vehiculo('Blanco', 4)
+print(vehiculo)
+
+# Segundo objeto, pero ahora de la clase Auto:
+auto = Auto("Blanco", 4, 120)
+print(auto)
+
+# Tercer objeto, de la clase hija Bicicleta:
+bici = Bicicleta("Amarillo", 2, 'BMX')
+print(bici)
