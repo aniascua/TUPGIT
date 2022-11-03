9ae571139df037caa8b56caa1e881df2b3c5c6b7
ANA ASCUA <anaascua5@yahoo.com.ar>
2022-11-03 13:24:33 -0300

    Clase 9 de Java con herencia

---
+++ Java/Lección 6/Herencia/src/domain/Empleado.java
@@ -0,0 +1,40 @@
+
+package domain;
+
+public class Empleado extends Persona { // extends: empleado es clase hija de la clase Persona
+    private int idEmpleado;
+    private double sueldo;
+    private static int contadorEmpleados; // es para incrementar
+    
+    // Constructor
+    public Empleado(String nombre, double sueldo) {
+        super(nombre);
+        this.idEmpleado = ++Empleado.contadorEmpleados;
+        this.sueldo = sueldo;
+    }
+
+    public int getIdEmpleado() {
+        return this.idEmpleado;
+    }
+
+    public double getSueldo() {
+        return this.sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override // con StringBuilder se puede modificar y compila más rapido/eficaz. sin esto, sucede todo lo contrario
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Empleado{idEmpleado=").append(idEmpleado);
+        sb.append(", sueldo=").append(sueldo);
+        sb.append(", ").append(super.toString());
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+    
+}
