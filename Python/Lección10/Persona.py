36cd36ab083464cece6459e418bc9b240099ab4e
ANA ASCUA <anaascua5@yahoo.com.ar>
2022-12-01 19:59:06 -0300

    Agregando el archivo diseño_clase UXF a la clase 14 de Python

--- Python/Lección10/Persona.py
+++ Python/Lección10/Persona.py
@@ -1,9 +1,15 @@
 class Persona:
     contador_personas = 0  # variable de clase
 
+    @classmethod
+    def generar_siguiente_valor(cls):
+        cls.contador_personas += 1
+        return cls.contador_personas
+
     def __init__(self, nombre, edad):
-        Persona.contador_personas += 1  # vamos incrementando
-        self.id_persona = Persona.contador_personas  # asignamos el incremento cada vez que comienza una nueva Persona
+        # Persona.contador_personas += 1  # vamos incrementando
+        # self.id_persona = Persona.contador_personas  # asignamos el incremento cada vez que comienza una nueva Persona
+        self.id_persona = Persona.generar_siguiente_valor()
         self.nombre = nombre
         self.edad = edad
 
@@ -18,5 +24,8 @@ persona2 = Persona('Pepito', 22)
 print(persona2)
 persona3 = Persona('ElTercerId', 50)
 print(persona3)
+Persona.generar_siguiente_valor()
+persona4 = Persona('Pepito', 25)
+print(persona4)
 print('')  # Espacio en blanco para salto de línea
 print(f'Valor del contador de personas: {Persona.contador_personas}')
