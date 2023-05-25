0a37b3f38df5ca4503779bf4fdb5311c2450ce36
ANA ASCUA <anaascua5@yahoo.com.ar>
2023-05-25 13:53:11 -0300

    Agrego clase de Python: Capa de datos, acceso datos

---
+++ Tecnicatura3Python/Lección07/capa_datos_persona/logger_base.py
@@ -0,0 +1,23 @@
+# Python clase 7 - capa de datos - logger base
+# 22/05/2023
+
+import logging as log
+
+# docs.python.org/3/howto/logging.html
+# Llamamos a una configuración básica
+
+log.basicConfig(level=log.DEBUG,
+                format='%(asctime)s:%(levelname)s [%(filename)s:%(lineno)s] %(message)s',
+                datefmt='%I:%M:%S %p',
+                handlers=[
+                    log.FileHandler('capa_datos_persona.log'),
+                    log.StreamHandler()
+                ])
+
+
+if __name__ == '__main__':
+    log.debug('Mensaje a nivel debug')
+    log.info('Mensaje a nivel info')
+    log.warning('Mensaje a nivel warning')
+    log.error('Mensaje a nivel error')
+    log.critical('Mensaje a nivel critical')
