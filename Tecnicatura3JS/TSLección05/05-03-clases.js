b79d532ebcf3b249bc2bf69f6b86dcd0e0d68b12
ANA ASCUA <anaascua5@yahoo.com.ar>
2023-05-25 13:55:16 -0300

    Agrego clase JS: 05-03-clases.js, index.html

--- Tecnicatura3JS/TSLección05/05-03-clases.js
+++ Tecnicatura3JS/TSLección05/05-03-clases.js
@@ -2,7 +2,7 @@
 
 //let persona3 = new Persona('Carla', 'Ponce'); Esto no se debe hacer: Persona is not defined
 
-class Persona {
+class Persona { // Clase padre
     constructor (nombre, apellido, email) {
         this._nombre = nombre;
         this._apellido = apellido;
@@ -25,7 +25,12 @@ class Persona {
     }   
 }
 
-class Empleado extends Persona {
+nombreCompleto(){
+    return this._nombre+' '+this._apellido+', '+this._departamento;
+    }
+}
+
+class Empleado extends Persona { // Clase hija
     constructor(nombre, apellido, departamento){
         super(nombre, apellido);
         this._departamento = departamento;
@@ -38,6 +43,18 @@ class Empleado extends Persona {
     set departamento(departamento){
         this._departamento = departamento;
     }   
+}
+
+// Sobreescritura
+nombreCompleto(){
+    return super.nombreCompleto+', '+this._departamento;
+}
+
+// Sobreescribiendo el método de la clase padre (Object)
+toString(){ // Regresa un String
+    // Se aplica el polimorfismo que significa = múltiples formas en tiempo de ejecución
+    // El método que se ejecuta depende si es una referencia de tipo padre o hija
+    return this.nombreCompleto();
 }
 
 let persona1 = new Persona('Juan', 'Perez');
@@ -53,4 +70,8 @@ console.log(persona2.nombre);
 
 let empleado1 = new Empleado('María', 'Jiménez', 'Sistemas');
 console.log(empleado1);
-console.log(empleado1.nombre);
+console.log(empleado1.nombreCompleto());
+
+// Object.prototype.toString // Esta es la manera de acceder a atributos y métodos de manera dinámica
+console.log(empleado1.toString);
+console.log(persona1.toString());
