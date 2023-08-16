8ddcaf1a2ac68f8ebe26cacf18078242e774354f
ANA ASCUA <anaascua5@yahoo.com.ar>
2023-08-16 15:02:20 -0300

    Update usuario_dao.py

--- Tecnicatura3Python/Lección07/capa_datos_persona/usuario_dao.py
+++ Tecnicatura3Python/Lección07/capa_datos_persona/usuario_dao.py
@@ -52,6 +52,23 @@ class UsuarioDAO:
             return cursor.rowcount
 
 
+if __name__ == '__main__':
+    # Eliminar usuario
+    usuario = Usuario(id_usuario=3)
+    usuario_eliminado = UsuarioDAO.eliminar(usuario)
+    log.debug(f'Usuario eliminado: {usuario_eliminado}')
 
+    # Actualizar usuario
+    usuario = Usuario(id_usuario=1, username='', password='369')
+    usuario_modificado = UsuarioDAO.actualizar(usuario)
+    log.debug(f'Usuario modificado: {usuario_modificado}')
 
+    # Insertar usuario
+    usuario = Usuario(username='kely', password='321')
+    usuario_insertado = UsuarioDAO.insertar(usuario)
+    log.debug(f'Usuario insertado: {usuario_insertado}')
 
+    # Listar o seleccionar
+    usuarios = UsuarioDAO.seleccionar()
+    for usuario in usuarios:
+        log.debug(usuario)
