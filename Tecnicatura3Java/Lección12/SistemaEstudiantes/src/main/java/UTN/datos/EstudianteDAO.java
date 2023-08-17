7e02bd02c9c6d7d4d26b490cf9e2d4f4649ff6f4
ANA ASCUA <anaascua5@yahoo.com.ar>
2023-08-17 19:18:31 -0300

    JAVA: creamos package 'presentacion' y rename Main to SistemaEstudiantesApp

--- Tecnicatura3Java/Lección12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
+++ Tecnicatura3Java/Lección12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -4,16 +4,17 @@ package UTN.datos;
 import UTN.dominio.Estudiante;
 
 import static UTN.conexion.Conexion.getConnection;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-        // 12.3 Comenzamos con las pruebas del método listarEstudiantes() -> solucionamos algunos errores de conexión
+// 12.3 Comenzamos con las pruebas del método listarEstudiantes() -> solucionamos algunos errores de conexión
 public class EstudianteDAO {
     // Método Listar:
-    public List<Estudiante> listarEstudiantes(){
+    public List<Estudiante> listarEstudiantes() {
         List<Estudiante> estudiantes = new ArrayList<>();
         // Creamos algunos objetos que son necesarios para comunicarnos con la base de datos:
         PreparedStatement ps; // Ayuda a preparar la sentencia SQL y la envia a la base de datos
@@ -28,10 +29,10 @@ public class EstudianteDAO {
         try {
             ps = con.prepareStatement(sql);
             rs = ps.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 var estudiante = new Estudiante();
                 estudiante.setIdEstudiante(rs.getInt("idestudiantes2023"));
-        // 12.2 Listado de estudiantes con la Clase EstudianteDAO -> Parte 4
+                // 12.2 Listado de estudiantes con la Clase EstudianteDAO -> Parte 4
                 estudiante.setNombre(rs.getString("nombre"));
                 estudiante.setApellido(rs.getString("apellido"));
                 estudiante.setTelefono(rs.getString("telefono"));
@@ -40,10 +41,9 @@ public class EstudianteDAO {
                 // Falta agregarlo a la lista
                 estudiantes.add(estudiante);
             }
-        } catch (Exception e){
-            System.out.println("Ocurrió un error al seleccionar datos: "+e.getMessage());
-        }
-        finally {
+        } catch (Exception e) {
+            System.out.println("Ocurrió un error al seleccionar datos: " + e.getMessage());
+        } finally {
             try {
                 con.close();
             } catch (Exception e) {
@@ -54,7 +54,7 @@ public class EstudianteDAO {
     } // Fin método listar
 
     // 12.4 Creamos en PersonaDAO() el Método por id -> FIND BY ID (ENCONTRAR MÉTODO POR ID):
-    public boolean buscarEstudiantePorID(Estudiante estudiante){
+    public boolean buscarEstudiantePorID(Estudiante estudiante) {
         PreparedStatement ps;
         ResultSet rs;
         Connection con = getConnection();
@@ -63,15 +63,15 @@ public class EstudianteDAO {
             ps = con.prepareStatement(sql);
             ps.setInt(1, estudiante.getIdEstudiante());
             rs = ps.executeQuery();
-            if(rs.next()){
+            if (rs.next()) {
                 estudiante.setNombre(rs.getString("nombre"));
                 estudiante.setApellido(rs.getString("apellido"));
                 estudiante.setTelefono(rs.getString("telefono"));
                 estudiante.setEmail(rs.getString("email"));
                 return true; // Se encontró el estudiante
             }// Fin del if
-        } catch (Exception e){
-            System.out.println("Ocurrió un error al buscar el estudiante: "+e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Ocurrió un error al buscar el estudiante: " + e.getMessage());
         } // Fin del catch
         finally {
             try {
@@ -109,53 +109,75 @@ public class EstudianteDAO {
         return false; // No se pudo insertar el estudiante
     } // Fin del método agregarEstudiante()
 
-        // 12.8 Agregamos el método -> modificarEstudiante()
-        public boolean modificarEstudiante(Estudiante estudiante){
-            PreparedStatement ps;
-            Connection con = getConnection();
-            String sql = "UPDATE estudiantes2023 SET nombre = ?, apellido = ?, telefono = ?, email = ? WHERE idestudiantes2023 = ?";
+    // 12.8 Agregamos el método -> modificarEstudiante()
+    public boolean modificarEstudiante(Estudiante estudiante) {
+        PreparedStatement ps;
+        Connection con = getConnection();
+        String sql = "UPDATE estudiantes2023 SET nombre = ?, apellido = ?, telefono = ?, email = ? WHERE idestudiantes2023 = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, estudiante.getNombre());
+            ps.setString(2, estudiante.getApellido());
+            ps.setString(3, estudiante.getTelefono());
+            ps.setString(4, estudiante.getEmail());
+            ps.setInt(5, estudiante.getIdEstudiante());
+            ps.execute();
+            return true; // Se modificó el estudiante
+        } catch (Exception e) {
+            System.out.println("Error al modificar el estudiante: " + e.getMessage());
+        } // Fin catch
+        finally {
             try {
-                ps = con.prepareStatement(sql);
-                ps.setString(1, estudiante.getNombre());
-                ps.setString(2, estudiante.getApellido());
-                ps.setString(3, estudiante.getTelefono());
-                ps.setString(4, estudiante.getEmail());
-                ps.setInt(5, estudiante.getIdEstudiante());
-                ps.execute();
-                return true; // Se modificó el estudiante
+                con.close();
             } catch (Exception e) {
-                System.out.println("Error al modificar el estudiante: "+e.getMessage());
+                System.out.println("Error al cerrar la conexión: " + e.getMessage());
             } // Fin catch
-            finally {
-                try {
-                    con.close();
-                } catch (Exception e) {
-                    System.out.println("Error al cerrar la conexión: "+e.getMessage());
-                } // Fin catch
-            } // Fin finally
-            return false; // No se pudo modificar el estudiante
-        } // Fin método modificarEstudiante()
+        } // Fin finally
+        return false; // No se pudo modificar el estudiante
+    } // Fin método modificarEstudiante()
 
 
-        public static void main(String[] args) {
+    // Método Eliminar Estudiante:  16/08/2023 (primera clase 4to semestre)
+    public boolean eliminarEstudiante(Estudiante estudiante) {
+        PreparedStatement ps;
+        Connection con = getConnection();
+        String sql = "DELETE FROM estudiantes2023 WHERE idestudiantes2023=?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, estudiante.getIdEstudiante());
+            ps.execute();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error al eliminar estudiante: " + e.getMessage());
+        } finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error al cerrar la conexión: " + e.getMessage());
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
         var estudianteDao = new EstudianteDAO();
         // 12.9 Comenzamos con las pruebas del método -> modificarEstudiante()
         var estudianteModificado = new Estudiante(1, "Juan Carlos", "Juarez", "12345678", "juan@mail.com");
         var modificado = estudianteDao.modificarEstudiante(estudianteModificado);
-        if(modificado)
-            System.out.println("Estudiante modificado: "+estudianteModificado);
+        if (modificado)
+            System.out.println("Estudiante modificado: " + estudianteModificado);
         else
-            System.out.println("No se modificó al estudiante: "+estudianteModificado);
+            System.out.println("No se modificó al estudiante: " + estudianteModificado);
 
 
-        // 12.7 Comenzamos con las pruebas del Método -> agregarEstudiante() Parte 1 y 2
-        // Agregar un estudiante
-//        var nuevoEstudiante = new Estudiante("Carlos", "Lara", "12345678", "carloslara@mail.com");
-//        var agregado = estudianteDao.agregarEstudiante(nuevoEstudiante);
-//        if(agregado)
-//            System.out.println("Estudiante agregado: "+nuevoEstudiante);
-//        else
-//            System.out.println("No se pudo agregar al estudiante: "+nuevoEstudiante);
+//      12.7 Comenzamos con las pruebas del Método -> agregarEstudiante() Parte 1 y 2
+//      Agregar un estudiante
+//      var nuevoEstudiante = new Estudiante("Carlos", "Lara", "12345678", "carloslara@mail.com");
+//      var agregado = estudianteDao.agregarEstudiante(nuevoEstudiante);
+//      if(agregado)
+//          System.out.println("Estudiante agregado: "+nuevoEstudiante);
+//      else
+//          System.out.println("No se pudo agregar al estudiante: "+nuevoEstudiante);
 
 
         // Listar los estudiantes
@@ -163,16 +185,16 @@ public class EstudianteDAO {
         List<Estudiante> estudiantes = estudianteDao.listarEstudiantes();
         estudiantes.forEach(System.out::println); // Función LAMBDA para imprimir los estudiantes
 
-        // 12.7 Comenzamos con las pruebas del Método -> agregarEstudiante() Parte 1 y 2
-        // Agregar un estudiante
-        //var nuevoEstudiante = new Estudiante("Carlos", "Lara", "12345678", "carloslara@mail.com");
-        //var agregado = estudianteDao.agregarEstudiante(nuevoEstudiante);
-        //if(agregado)
-        //    System.out.println("Estudiante agregado: "+nuevoEstudiante);
-        //else
-        //    System.out.println("No se pudo agregar al estudiante: "+nuevoEstudiante);
+//        12.7 Comenzamos con las pruebas del Método -> agregarEstudiante() Parte 1 y 2
+//        Agregar un estudiante
+//        var nuevoEstudiante = new Estudiante("Carlos", "Lara", "12345678", "carloslara@mail.com");
+//        var agregado = estudianteDao.agregarEstudiante(nuevoEstudiante);
+//        if(agregado)
+//        System.out.println("Estudiante agregado: "+nuevoEstudiante);
+//        else
+//        System.out.println("No se pudo agregar al estudiante: "+nuevoEstudiante);
 
-        // 12.5 Hacemos las pruebas del método -> Buscar un estudiante por ID()
+//        12.5 Hacemos las pruebas del método -> Buscar un estudiante por ID()
 //        var estudiante1 = new Estudiante(1);
 //        System.out.println("Estudiantes antes de la búsqueda: "+estudiante1);
 //        var encontrado = estudianteDao.buscarEstudiantePorID(estudiante1);
@@ -180,5 +202,27 @@ public class EstudianteDAO {
 //            System.out.println("Estudiante encontrado: "+estudiante1);
 //        else
 //            System.out.println("No se encontró al estudiante: "+estudiante1.getIdEstudiante());
+
+
+
+        // Eliminar estudiante con ID 3:   // AGREGADO 16/08/2023 4to semestre
+        var estudianteEliminar = new Estudiante(3);
+        var eliminado = estudianteDao.eliminarEstudiante(estudianteEliminar);
+        if(eliminado)
+            System.out.println("Estudiante eliminado: "+estudianteEliminar);
+        else
+            System.out.println("No se eliminó el estudiante: "+estudianteEliminar);
+
+        // Listar a los estudiantes
+        System.out.println("Listado de estudiantes: ");
+        List<Estudiante> estudiantes1= estudianteDao.listarEstudiantes();
+        estudiantes.forEach(System.out::println); // función LAMBDA para imprimir
+
+        // Buscar por ID
+        // var estudiante1 = new Estudiante(1);
+        //System.out.println("Estudiantes antes de la búsqueda: "+estudiantes1);
+        //var encontrado = estudianteDao.buscarEstudiantePorId(estudiantes1);
+        //if(encontrado)
+        //     System.out.println("No se encontró el estudiante: "+estudiantes1+.getIdEstudiante());
     }
 }
---
+++ Tecnicatura3Java/Lección12/SistemaEstudiantes/src/main/java/UTN/presentacion/SistemaEstudiantesApp.java
@@ -0,0 +1,131 @@
+package UTN.presentacion;
+
+import UTN.datos.EstudianteDAO;
+import UTN.dominio.Estudiante;
+
+import java.util.Scanner;
+
+public class SistemaEstudiantesApp {
+    public static boolean main(String[] args) {
+        var salir = false;
+        var consola = new Scanner(System.in); // Para leer información de la consola
+        // Se crea una instancia de la clase servicio, esto lo hacemos fuera del ciclo
+        var estudianteDao = new EstudianteDAO(); // Esta instancia debe hacerse una vez
+        while (!salir) {
+            try {
+                mostrarMenu(); // Mostramos el menú
+                // Este será el método ejecutarOpciones que devolverá un booleano
+                salir = ejecutarOpciones(consola, estudianteDao); // Este arroja una exception
+            } catch (Exception e) {
+                System.out.println("Ocurrió un error al ejecutar la operacion: " + e.getMessage());
+            }
+        } // Fin del ciclo while
+    } // Fin del main
+
+
+            private static void mostrarMenu () {
+                System.out.println("""
+                        ****** Sistema de Estudiantes ******
+                        1. Listar estudiantes
+                        2. Buscar estudiantes
+                        3. Agregar estudiante
+                        4. Modificar estudiante
+                        5. Eliminar estudiante
+                        6. Salir
+                        Elegí una opción: 
+                        """);
+            }
+
+        /* Método para ejecutar las opciones, va a regresar un valor booleano,
+           ya que es el que puede modificar el valor de la variable Salir,
+           si es verdadero termina el ciclo while
+        */
+            private static boolean ejecutarOpciones (Scanner consola, EstudianteDAO estudianteDAO){
+                var opcion = Integer.parseInt(consola.nextLine());
+                var salir = false;
+                switch (opcion) {
+                    case 1 -> { // Listar estudiantes
+                        System.out.println("Listado de Estudiantes...");
+                        // no muestra la información, solo recupera la info y regresa una lista
+                        var estudiantes = estudianteDao.listarEstudiantes();// recibe el listado
+                        // vamos a iterar cada objeto de tipo estudiante
+                        estudiantes.forEach(System.out::println);// para imprimir la lista
+                    } // Fin case 1
+                    case 2 -> { // Buscar estudiante por ID
+                        System.out.println("Introduzca el ID_estudiante a buscar: ");
+                        var idEstudiante = Integer.parseInt(consola.nextLine());
+                        var estudiante = new Estudiante(idEstudiante);
+                        var encontrado = estudianteDao.buscarEstudiantePorID(estudiante);
+                        if (encontrado)
+                            System.out.println("Estudiante encontrado: " + estudiante);
+                        else
+                            System.out.println("Estudiante NO encontrado: " + estudiante);
+                    }// fin case 2
+                    case 3 -> { // Agregar estudiante
+                        System.out.println("Agregar estudiante: ");
+                        System.out.println("Nombre: ");
+                        var nombre = consola.nextLine();
+                        System.out.println("Apellido: ");
+                        var apellido = consola.nextLine();
+                        System.out.println("Telefono: ");
+                        var telefono = consola.nextLine();
+                        System.out.println("Email: ");
+                        var email = consola.nextLine();
+                        // crear objeto estudiante (SIN ID)
+                        var estudiante = new Estudiante(nombre, apellido, telefono, email);
+                        var agregado = estudianteDao.agregarEstudiante(estudiante);
+                    else
+                        System.out.println("Estudiante NO agregado: " + estudiante);
+                    } // fin case 3
+                    case 4 -> { // Modificar estudiante
+                        System.out.println("Modificar estudiante: ");
+                        // Acá lo primero es especificar cual es el ID del estudiante a modificar
+                        System.out.println("ID del estudiante a modificar: ");
+                        var idEstudiante = Integer.parseInt(consola.nextLine());
+                        System.out.println("Nombre: ");
+                        var nombre = consola.nextLine();
+                        System.out.println("Apellido: ");
+                        var apellido = consola.nextLine();
+                        System.out.println("Telefono: ");
+                        var telefono = consola.nextLine();
+                        System.out.println("Email: ");
+                        var email = consola.nextLine();
+                        // crea el objeto estudiante a modificar
+                        var estudiante =
+                                new Estudiante(idEstudiante, nombre, apellido, telefono, email);
+                        var modificado = estudianteDao.modificarEstudiante(estudiante);
+                        if (modificado)
+                            System.out.println("Estudiante modificado: " + estudiante);
+                        else
+                            System.out.println("Estudiante NO modificado: " + estudiante);
+                    } // fin case 4
+                    case 5 -> { // Eliminar estudiante
+                        System.out.println("Eliminar estudiante: ");
+                        System.out.println("ID del estudiante: ");
+                        var idEstudiante = Integer.parseInt(consola.nextLine());
+                        var estudiante = new Estudiante(idEstudiante);
+                        var eliminado = estudianteDao.eliminarEstudiante(estudiante);
+                        if (eliminado)
+                            System.out.println("Estudiante eliminado: " + estudiante);
+                        else
+                            System.out.println("Estudiante NO eliminado: " + estudiante);
+                    } // Fin case 5
+                    case 6 -> { // Salir
+                        System.out.println("Hasta pronto!");
+                        salir = true;
+                    }// fin caso 6
+                    default -> System.out.println("Opción no reconocida, ingrese de nuevo: ");
+                }// fin switch
+                return salir;
+            }
+
+
+        /*var conexion = Conexion.getConnection();
+        if(conexion != null)
+            System.out.println("Conexión exitosa" +conexion);
+        else
+            System.out.println("Conexión fallida");*/
+
+
+        } // Fin main
+    } // Fin clase Main
