0e5fe1f1a1f0b18aa2766f9433294d9f57452799
Ana Ascua <anaascua5@yahoo.com.ar>
2023-10-03 18:06:08 -0300

    mas cambios en tareas.controller.js

--- PERN-stack/src/controllers/tareas.controller.js
+++ PERN-stack/src/controllers/tareas.controller.js
@@ -1,27 +1,33 @@
 import { pool } from "../db.js";
 
-export const listarTareas = async (req, res, next) => {
-        const resultado = await pool.query("SELECT * FROM tareas1");
+export const listarTareas = async (req, res) => {
+        const resultado = await pool.query("SELECT * FROM tareas");
         console.log(resultado);
         return res.json(resultado.rows);
 }
 
-export const listarTarea = (req, res) => res.send('obteniendo tarea única');
+export const listarTarea = async(req, res) => {
+    const resultado = await pool.query("SELECT * FROM tareas WHERE id = $1", [req.params.id]);
+}
 
-export const crearTarea = (req, res, next) => {
+
+export const crearTarea = async (req, res, next) => {
     const { titulo, desc } = req.body;
 
     try {
         const result = await pool.query('INSERT INTO tareas (titulo, desc) VALUES ($1, $2) RETURNING *', [titulo, desc]);
         res.json(result.rows);
-    } catch (e) {
+    } catch (error) { 
         if (error.code === "23505") {
-            return res.send("ya existe una tarea con ese título");
+            return res.status(409).json({ 
+                message: "Ya existe una tarea con ese título"
+            });
         }
-        console.log(Error);
+        console.error(error);
         next(error);
     }
 }
+
 
 export const actualizarTarea = (req, res) => res.send('actualizando tarea única');
 
