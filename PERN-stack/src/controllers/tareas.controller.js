cd8a13ef1c220defea4fa5731510fbd2e8f17a24
Ana Ascua <anaascua5@yahoo.com.ar>
2023-10-03 18:13:40 -0300

    modifico eliminarTarea y agrego un try catch

--- PERN-stack/src/controllers/tareas.controller.js
+++ PERN-stack/src/controllers/tareas.controller.js
@@ -1,9 +1,9 @@
 import { pool } from "../db.js";
 
 export const listarTareas = async (req, res) => {
-        const resultado = await pool.query("SELECT * FROM tareas");
-        console.log(resultado);
-        return res.json(resultado.rows);
+    const resultado = await pool.query("SELECT * FROM tareas");
+    console.log(resultado);
+    return res.json(resultado.rows);
 }
 
 export const listarTarea = async (req, res) => {
@@ -18,9 +18,9 @@ export const crearTarea = async (req, res, next) => {
     try {
         const result = await pool.query('INSERT INTO tareas (titulo, desc) VALUES ($1, $2) RETURNING *', [titulo, desc]);
         res.json(result.rows);
-    } catch (error) { 
+    } catch (error) {
         if (error.code === "23505") {
-            return res.status(409).json({ 
+            return res.status(409).json({
                 message: "Ya existe una tarea con ese título"
             });
         }
@@ -33,6 +33,20 @@ export const crearTarea = async (req, res, next) => {
 export const actualizarTarea = (req, res) => res.send('actualizando tarea única');
 
 export const eliminarTarea = async (req, res) => {
-    const resultado = await pool.query("DELETE FROM tareas WHERE id = $1", [req.params.id]);
-    console.log(resultado);
+    try {
+        const resultado = await pool.query("DELETE FROM tareas WHERE id = $1", [req.params.id]);
+
+        if (resultado.rowCount === 0) {
+            return res.status(404).json({
+                message: "No existe una tarea con ese ID"
+            });
+        }
+
+        return res.sendStatus(204);
+    } catch (error) {
+        console.error("Error al eliminar la tarea:", error);
+        return res.status(500).json({
+            message: "Error interno del servidor al eliminar la tarea"
+        });
+    }
 }
