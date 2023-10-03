d5e9659d50a9993e40f1e90ca1464fb9cdabd535
Ana Ascua <anaascua5@yahoo.com.ar>
2023-10-02 21:49:13 -0300

    agregando modificaciones de clase 6, STACK-pern

--- PERN-stack/src/controllers/tareas.controller.js
+++ PERN-stack/src/controllers/tareas.controller.js
@@ -1,9 +1,24 @@
+import { pool } from "../db.js";
+
 export const listarTareas = (req, res) => res.send('obteniendo tareas');
 
 export const listarTarea = (req, res) => res.send('obteniendo tarea única');
 
-export const crearTarea = (req, res) => res.send('creando tarea');
+export const crearTarea = (req, res, next) => {
+    const { titulo, desc } = req.body;
 
-export const actualizarTarea =  (req, res) => res.send('actualizando tarea única');
+    try {
+        const result = await pool.query('INSERT INTO tareas (titulo, desc) VALUES ($1, $2) RETURNING *', [titulo, desc]);
+        res.json(result.rows);
+    } catch (e) {
+        if (error.code === "23505") {
+            return res.send("ya existe una tarea con ese título");
+        }
+        console.log(Error);
+        next(error);
+    }
+}
+
+export const actualizarTarea = (req, res) => res.send('actualizando tarea única');
 
 export const eliminarTarea = (req, res) => res.send('eliminando tarea única');
