a6d24531d3d9e53d2f08a4849925554ff789648e
Ana Ascua <anaascua5@yahoo.com.ar>
2023-10-03 20:12:16 -0300

    Python: uso del método format() parte 1 y 2

--- Python 4to semestre/Clase01/def_formato_str.py
+++ Python 4to semestre/Clase01/def_formato_str.py
@@ -3,4 +3,18 @@
 nombre = 'Ana'
 edad = 27
 mensaje_con_formato = 'Mi nombre es %s y tengo %d años' % (nombre, edad)
-print(mensaje_con_formato)
+
+
+# Creamos una tupla
+persona = ('Carla', 'Gomez', 5000.00)
+mensaje_con_formato = 'Hola %s %s . Tu sueldo es: %.2f' # % persona # Acá le pasamos el objeto tupla
+print(mensaje_con_formato % persona)
+
+nombre = 'Juan'
+edad = 19
+sueldo = 3000
+# mensaje_con_formato = 'Nombre {} Edad {} Sueldo {:.2f}'.format(nombre, edad, sueldo)
+# print(mensaje_con_formato)
+
+mensaje = 'Nombre {0} Edad {1} Sueldo {:.2f}'.format(nombre, edad, sueldo)
+print(mensaje)
