c923db75c05fd54b2286c1dcc728eb57f86fe8c7
Ana Ascua <anaascua5@yahoo.com.ar>
2023-10-03 20:20:54 -0300

    Python: uso del template literal, placeholders en interpolación {}

---
+++ Python 4to semestre/Clase01/f_string.py
@@ -0,0 +1,6 @@
+# En la interpolación, a los { } se les dice Placeholder, y no se les dice llaves
+nombre = 'Pepe'
+edad = 28
+sueldo = 3000
+mensaje = f'Nombre {nombre} Edad {edad} Sueldo {sueldo:.2f}'
+print(mensaje)
