6ee6973e326cfbc3d5c97ebbf155c4721df806c5
Ana Ascua <anaascua5@yahoo.com.ar>
2023-10-03 21:38:57 -0300

    configurando sign in & sign up

--- PERN-stack/src/controllers/auth.controller.js
+++ PERN-stack/src/controllers/auth.controller.js
@@ -1,4 +1,14 @@
-export const signin = (req, res) => ("perfil de usuario");
-export const signup = (req, res) => ("registrando"); 
+import { pool } from "../db.js"
+
+export const signin = (req, res) => res.send("ingresando");
+
+export const signup = async (req, res) => {
+    const (name, email, pass) = req.body;
+    res.send("registrando");
+
+    const result = await pool.query("INSERT INTO users (name, email, pass) VALUES ($1, $2, $3)", [name, email, pass])
+};
+
 export const signout = (req, res) => ("cerrando sesión");
+
 export const profile = (req, res) => ("perfil de usuario");
