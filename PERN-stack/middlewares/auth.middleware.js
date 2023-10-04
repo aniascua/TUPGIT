3c663d8531a2bcd1b0b53c535826478433e60406
Ana Ascua <anaascua5@yahoo.com.ar>
2023-10-03 22:15:47 -0300

    import jwt, token verification

--- PERN-stack/middlewares/auth.middleware.js
+++ PERN-stack/middlewares/auth.middleware.js
@@ -1,5 +1,21 @@
-export const isAuth = (req, res, next) => {
-    console.log(req.cookies);
+import { Jwt } from "jsonwebtoken";
 
-    next();
+export const isAuth = (req, res, next) => {
+    console.log(req.cookies.token);
+
+    if (!token) {
+        return res.status(401).json({
+            message: "No estás autorizado."
+        });
+    }
+
+    jwt.verify(token, "xyz123", (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                message: "No estás autorizado"
+            });
+        }
+        req.userId = decoded.id;
+        next();
+    });
 };
--- PERN-stack/schemas/app.js
+++ PERN-stack/schemas/app.js
@@ -2,11 +2,14 @@ import express from 'express';
 import morgan from 'morgan';
 import tareasRoutes from "../src/router/tareas.routes.js"
 import authRoutes from "../src/router/auth.routes.js"
+import cookieParser from "cookie-parser";
+
 
 const app = express();
 
 // Middleware: convierte datos que llegan al backend en obj de js
 app.use(morgan("dev"));
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
