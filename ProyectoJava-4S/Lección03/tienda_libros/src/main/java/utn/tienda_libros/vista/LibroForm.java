b64748a0ab89c46aa60022254c728169805c1bcd
Ana Ascua <anaascua5@yahoo.com.ar>
2023-10-05 00:35:52 -0300

    JAVA: clase 8, continuamos avanzando con este proyecto en Java, con SpringBoot y Swing

--- ProyectoJava-4S/Lección03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroForm.java
+++ ProyectoJava-4S/Lección03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroForm.java
@@ -1,11 +1,14 @@
 package tienda_libros.vista;
 
 import org.springframework.beans.factory.annotation.AutoWired;
-import tienda_libros.servicio.LibroServicio;
+import org.springframework.stereotype.Component;
 import utn.tienda_libros.servicio.LibroServicio;
 
 import javax.swing.*;
+import java.awt.*;
 
+
+@Component
 public class LibroForm extends JFrame {
     LibroServicio libroServicio;
     private JPanel panel;
@@ -21,6 +24,11 @@ public class LibroForm extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setSize(900, 700);
+        // para obtener las dimensiones de la ventana
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension tamanioPantalla = toolkit.getScreenSize();
+        int x = (tamanioPantalla.width - getWidth()/2);
+        int y = (tamanioPantalla.height - getHeight()/2);
+        setLocation(x, y);
     }
-
 }
