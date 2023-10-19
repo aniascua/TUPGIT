4675905d6c30185f69f2938b683ff32b44500e20
aniascua <83845164+aniascua@users.noreply.github.com>
2023-10-18 21:54:01 -0300

    Node.js: Clase 9, callbackHablar - 1

--- Node-4S/02-Fundamentos_Node/callback.js
+++ Node-4S/02-Fundamentos_Node/callback.js
@@ -5,6 +5,13 @@ function hola(nombre, miCallback) {
     }, 1000);
 }
 
+function hablar(callbackHablar) {
+    setTimeout( function () {
+        console.log("bla bla bla bla...");
+        callbackHablar();
+    }, 1000);
+}
+
 function adios(nombre, otroCallback) {
     setTimeout( function() {
         console.log('Adios', nombre);
