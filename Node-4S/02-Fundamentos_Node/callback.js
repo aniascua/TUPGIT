a5c7d5bc5b806e66eba3fa5ca90ddd304c9ebf1b
aniascua <83845164+aniascua@users.noreply.github.com>
2023-10-18 21:45:32 -0300

    Node.js: Clase 8 - Callbacks 2

--- Node-4S/02-Fundamentos_Node/callback.js
+++ Node-4S/02-Fundamentos_Node/callback.js
@@ -1,8 +1,10 @@
 function soyAsincrona() {
-    console.log('Hola, soy una función asíncrona');
+    setTimeout( function (miCallback) {
+            console.log('Hola, soy una función asíncrona');
+    }, 1000);
 }
 
 console.log('Iniciando el proceso...');
-soyAsincrona();
-console.log('Terminando el proceso...');
-
+soyAsincrona(function() {
+    console.log('Terminando el proceso...');
+});
